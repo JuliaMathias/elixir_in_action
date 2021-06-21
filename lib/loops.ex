1aba26c7134398c6721a3a84f01207bea4cecfc1
JuliaMathias <juliamss@gmail.com>
2021-06-21 20:56:18 -0300

    add range and positive functions

--- lib/loops.ex
+++ lib/loops.ex
@@ -3,18 +3,32 @@ defmodule ElixirInAction.Loops do
   Loop exercises
   """
   @doc "function that calculates the length of a list"
-  @spec list_len(list :: list()) :: non_neg_integer
+  @spec list_len(list) :: non_neg_integer
   def list_len(list) do
     Kernel.length(list)
   end
 
   @doc "function that takes two integers, from and to , and returns a list of all
-numbers in the given range"
-  def range(_from, _to) do
+  numbers in the given range"
+  @spec range(from :: integer, to :: integer) :: list
+  def range(from, to) when from > to do
+    []
+  end
+
+  def range(from, to) do
+    [from | range(from + 1, to)]
   end
 
   @doc "function that takes a list and returns another list that contains only
-the positive numbers from the input list"
-  def positive(_list) do
+  the positive numbers from the input list"
+  @spec positive(list) :: list
+  def positive([]), do: []
+
+  def positive([head | tail]) when head > 0 do
+    [head | positive(tail)]
+  end
+
+  def positive([_ | tail]) do
+    positive(tail)
   end
 end
