c7bd3ec8754238c6671d4daa1f84eec3ac5c9712
JuliaMathias <juliamss@gmail.com>
2021-06-28 20:46:56 -0300

    feat: words_per_line! function

--- lib/streams.ex
+++ lib/streams.ex
@@ -29,9 +29,13 @@ defmodule ElixirInAction.Streams do
     |> Enum.take(1)
   end
 
-  #   @doc "function that returns a list of numbers, with each number rep-
-  # resenting the word count in a file. Hint: to get the word count of a line, use
-  # length(String.split(line)) ."
-  #   def words_per_line!(file) do
-  #   end
+  @doc "function that returns a list of numbers, with each number rep-
+  resenting the word count in a file. Hint: to get the word count of a line, use
+  length(String.split(line)) ."
+  def words_per_line!(file) do
+    File.stream!(file)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Stream.map_every(1, fn line -> length(String.split(line)) end)
+    |> Enum.to_list()
+  end
 end
