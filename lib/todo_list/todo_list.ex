3d9a82458d14b23109d883d9c4349dfd8aa9f99d
JuliaMathias <juliamss@gmail.com>
2021-07-05 20:50:34 -0300

    feat: use MultiDict

--- lib/todo_list/todo_list.ex
+++ lib/todo_list/todo_list.ex
@@ -5,22 +5,17 @@ defmodule ElixirInAction.TodoList do
 
   @doc "Initiates a new to-do list"
   @spec new :: %{}
-  def new(), do: %{}
+  def new(), do: MultiDict.new()
 
   @doc "Adds entry to the to-do list"
   @spec add_entry(map, Calendar.date(), String.t()) :: map
   def add_entry(todo_list, date, title) do
-    Map.update(
-      todo_list,
-      date,
-      [title],
-      fn titles -> [title | titles] end
-    )
+    MultiDict.add(todo_list, date, title)
   end
 
   @doc "Lists all to-do list entries"
   @spec entries(map, Calendar.date()) :: list
   def entries(todo_list, date) do
-    Map.get(todo_list, date, [])
+    MultiDict.get(todo_list, date)
   end
 end
