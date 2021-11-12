6d5e3345a5093d4de25cae2763335d2f1e7956c8
JuliaMathias <juliamss@gmail.com>
2021-11-12 20:56:55 -0300

    fwat: generic server

---
+++ lib/elixir_in_action/code/server_process.ex
@@ -0,0 +1,51 @@
+defmodule ElixirInAction.Code.ServerProcess do
+  @doc """
+  Starts the Generic Server.
+
+  ### Example
+
+  iex> ServerProcess.start(KeyValueStore)
+  #PID<0.202.0>
+  """
+  @spec start(atom) :: pid
+  def start(callback_module) do
+    spawn(fn ->
+      initial_state = callback_module.init()
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  @doc """
+  Sends a message to the server telling it to do something.
+
+  ### Example
+
+  iex> ServerProcess.call(pid, {:put, :some_key, :some_value})
+  :ok
+  iex> ServerProcess.call(pid, {:get, :some_key})
+  :some_value
+  """
+  @spec call(pid, tuple) :: any
+  def call(server_pid, request) do
+    send(server_pid, {request, self()})
+
+    receive do
+      {:response, response} ->
+        response
+    end
+  end
+
+  defp loop(callback_module, current_state) do
+    receive do
+      {request, caller} ->
+        {response, new_state} =
+          callback_module.handle_call(
+            request,
+            current_state
+          )
+
+        send(caller, {:response, response})
+        loop(callback_module, new_state)
+    end
+  end
+end
