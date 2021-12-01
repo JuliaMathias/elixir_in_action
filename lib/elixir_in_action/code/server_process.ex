a4530bf9dd5a6ab41d46f7bd63677a7d0d9ab3bf
julia mathias <juliamss@gmail.com>
2021-12-01 00:17:10 +0000

    implement async requests

--- lib/elixir_in_action/code/server_process.ex
+++ lib/elixir_in_action/code/server_process.ex
@@ -35,11 +35,22 @@ defmodule ElixirInAction.Code.ServerProcess do
     end
   end
 
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request})
+  end
+
   defp loop(callback_module, current_state) do
     receive do
       {:call, request, caller} ->
         {response, new_state} =
           callback_module.handle_call(
+            request,
+            current_state
+          )
+
+      {:cast, request} ->
+        new_state =
+          callback_module.handle_cast(
             request,
             current_state
           )
